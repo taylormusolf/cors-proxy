c578f9a459b5eb11a8e46254122a7832b42d96d0
Taylor Musolf <tcmusolf@gmail.com>
2023-07-20 17:27:30 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+if(process.env.NODE_ENV === 'development'){
+    const dotenv = require('dotenv');
+    dotenv.config();
+}
+
+const app = express();
+app.use(cors());
+
+// to bypass CORS
+app.get('/', async (req, res)=>{
+    if(req.query.url){
+        //query string with key of url is assume
+        console.log(`processing request to ${req.query.url}`)
+        const proxyRes = await axios.get(`${req.query.url}&apikey=${process.env.API_KEY}`);
+        res.send(proxyRes.data);
+    } else {
+        res.send('Please provide a url query param')
+    }
+});
+
+app.listen(5001, ()=> {
+    console.log('Listening on port 5001 ....')
+})
---
+++ why.md
@@ -0,0 +1,5 @@
+CORS - servers will not fulfill requests from other domains if CORS is disabled.
+
+CORS - Cross origin resource sharing
+
+Provide an env if you want to hide api keys
