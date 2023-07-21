4eb3a4cddfccd5b630763672100971c46b53e9a5
Taylor Musolf <tcmusolf@gmail.com>
2023-07-21 12:26:10 -0700

    Add chat gpt passthrough

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 
+const { Configuration, OpenAIApi } = require("openai");
+
 if(process.env.NODE_ENV === 'development'){
     const dotenv = require('dotenv');
     dotenv.config();
@@ -10,17 +12,37 @@ if(process.env.NODE_ENV === 'development'){
 const app = express();
 app.use(cors());
 
+//CHAT GPT route
+
+app.post('/chat', async (req, res)=>{
+    console.log(`processing chat request`)
+    const openai = new OpenAIApi(new Configuration({
+        apiKey: process.env.CHAT_API_KEY
+    }));
+    const res = await openai.createChatCompletion({
+        model: "gpt-4",
+        // model: "gpt-3.5-turbo",
+        messages: req.body.messages,
+        max_tokens: 150,
+        temperature: 0.9
+    });
+    return res.data.choices[0].message
+
+});
+
 // to bypass CORS
 app.get('/', async (req, res)=>{
     if(req.query.url){
         //query string with key of url is assume
         console.log(`processing request to ${req.query.url}`)
-        const proxyRes = await axios.get(`${req.query.url}&apikey=${process.env.API_KEY}`);
+        const proxyRes = await axios.get(`${req.query.url}`);
         res.send(proxyRes.data);
     } else {
         res.send('Please provide a url query param')
     }
 });
+
+
 
 app.listen(process.env.PORT || 5001, ()=> {
     console.log('Listening on port 5001 ....')
