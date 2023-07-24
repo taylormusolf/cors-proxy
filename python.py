aff02cafd2feb446de4749325027b3968bbbc779
Taylor Musolf <tcmusolf@gmail.com>
2023-07-24 16:31:03 -0700

    Python test

--- python.py
+++ python.py
@@ -1,3 +1,15 @@
+from youtube_transcript_api import YouTubeTranscriptApi
 import sys
-print({'var1': sys.argv[1], 'number': 1})
+tx = YouTubeTranscriptApi.get_transcript('Kw51fkRiKZU', languages=['en'])
+outls = []
+for i in tx:
+    outtxt = (i['text'])
+    outls.append(outtxt)
+
+full_transcript = " ".join(outls)
+
+data_to_pass_back = full_transcript
+output = data_to_pass_back
+print(output)
+# print({'var1': sys.argv[1], 'number': 1})
 sys.stdout.flush()
